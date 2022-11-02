a216f4ecadc0ece2fe48cb1bef70bef14ea67092
devyouth94 <devyouth94@gmail.com>
2022-11-03 03:02:16 +0900

    Feat: 광고 및 정기모임 설명 컴포넌트 구현

---
+++ src/components/Info.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Info = () => {
+  return (
+    <SInfoContainer>
+      <h1>정기모임</h1>
+      <p>
+        3개월 동안 네 번 만나는 정기 모임입니다.
+        <br />
+        12명의 새로운 사람들과 대화와 취향을 나눠요.
+      </p>
+    </SInfoContainer>
+  );
+};
+
+const SInfoContainer = styled.div`
+  padding: 3rem 2rem 0 2rem;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 700;
+  }
+
+  p {
+    margin-top: 2rem;
+
+    font-size: 1.4rem;
+    line-height: 2.2rem;
+  }
+`;
+
+export default Info;
