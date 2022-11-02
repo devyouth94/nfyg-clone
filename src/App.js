7dc73f843387c8f221275a086cef6734c4bfee75
devyouth94 <devyouth94@gmail.com>
2022-11-03 03:05:07 +0900

    Feat: App파일 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <div>App</div>;
-}
+import Header from "components/Header";
+import Advertisement from "components/Advertisement";
+import Info from "components/Info";
+import Introduce from "components/Introduce";
+import UpcomingList from "components/UpcomingList";
+import ProceedingList from "components/ProceedingList";
+
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <SLayout>
+      <Header />
+
+      <Advertisement />
+
+      <Info />
+
+      <Introduce />
+
+      <UpcomingList />
+
+      <ProceedingList />
+    </SLayout>
+  );
+};
+
+const SLayout = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+
+  width: 375px;
+`;
 
 export default App;
