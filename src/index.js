766ec11e01527962c501756ca815ab002b4dd456
devyouth94 <devyouth94@gmail.com>
2022-11-03 02:59:12 +0900

    Design: 전역 스타일 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import GlobalStyles from "styles/GlobalStyles";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+);
